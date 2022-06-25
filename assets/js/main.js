bbe4e7e47b1b2b1313377c70858ea14b0ab1fb99
Anthony Moran <anthony771998@gmail.com>
2022-06-25 18:13:32 -0400

    Replaced image and added link to poptrox caption

--- assets/js/main.js
+++ assets/js/main.js
@@ -99,7 +99,7 @@
 			$window.on('load', function() {
 
 				$('#two').poptrox({
-					caption: function($a) { return $a.next('h3').text(); },
+					caption: function($a) { return $a.next('h3').html(); },
 					overlayColor: '#2c2c2c',
 					overlayOpacity: 0.85,
 					popupCloserText: '',
